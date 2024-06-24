65802d9fcf6dcc58c4227e15b00bea866dbd5f73
KouMoh <126945657+KouMoh@users.noreply.github.com>
2024-06-24 20:41:26 +0530

    Add files via upload

--- BACKEND/express/index.js
+++ BACKEND/express/index.js
@@ -3,10 +3,25 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/",(req,res) =>{
-    res.send("<h1>Hello World</h1>");
+app.get("/", (req,res)=>{
+    const today = new Date();
+    const day =today.getDay();
+    // console.log(day);
+
+    let type = "a weekday";
+    let advice = "it's time to workhard";
+
+    if(day ==0 || day ==6){
+        type = "the weekend";
+        adv = "it's time to work hard!"
+    }
+res.render("index.ejs",{
+    dayType: "a weekday",
+    advice: "it's time to work hard"})
 });
 
+
+
 app.listen(port, () =>{
-    console.log(`Server running on ${port}`);
+    console.log(`Server is listening on port ${port}.`);
 })
