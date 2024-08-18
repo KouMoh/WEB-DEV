17002ee635c73618f36f89f45086683b16a5f21c
KouMoh <126945657+KouMoh@users.noreply.github.com>
2024-08-18 14:01:02 +0530

    Add files via upload

---
+++ BACKEND/Blog/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const articleRouter = require('./routes/articles')
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req,res)=>{
+    const articles = [{
+
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test description'
+    },
+    {
+
+        title: 'Test Article-2',
+        createdAt: new Date(),
+        description: 'Test description-2'
+    } 
+    ]
+    res.render('index', {articles: articles});
+})
+
+app.use('/articles', articleRouter)
+
+
+app.listen(5000 , (req,res)=>{
+    console.log("Listening at port 5000");
+    
+})
