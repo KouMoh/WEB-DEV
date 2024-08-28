f4101dc89bb23c656f0a68138c70fad15ddaa2b7
KouMoh <126945657+KouMoh@users.noreply.github.com>
2024-08-28 16:53:17 +0530

    Update index.js

--- BACKEND/INDIE PROJECTS/express_ejs/index.js
+++ BACKEND/INDIE PROJECTS/express_ejs/index.js
@@ -1,31 +1,58 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const path = require('path');
 
-app.set('view engine' ,'ejs')
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res)=>{
-    const messages = [
-        {
-          text: "Hi there!",
-          user: "Amando",
-          added: new Date()
-        },
-        {
-          text: "Hello World!",
-          user: "Charles",
-          added: new Date()
-        },{
-            text:"Architect of own Destruction",
-            user:"Amul",
-            added: new Date()
-        }
-      ];
-      res.render("index", { title: "Mini Messageboard", messages: messages })
-})
 
-app.listen(8080,(req,res)=>{
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    },
+    {
+        text: "Architect of own Destruction",
+        user: "Amul",
+        added: new Date()
+    }
+];
+
+app.get('/', (req, res) => {
+    res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+app.get('/new', (req, res) => {
+    res.render("form");
+});
+
+app.post("/new", (req, res) => {
+    const messageText = req.body.messageText;
+    const messageUser = req.body.messageUser;
+
+    messages.push({ text: messageText, user: messageUser, added: new Date() });
+    res.redirect("/");
+});
+
+
+app.get('/message/:id', (req, res) => {
+    const messageId = parseInt(req.params.id);
+    if (messageId >= 0 && messageId < messages.length) {
+        const message = messages[messageId];
+        res.render('message', { title: "Message Details", message: message });
+    } else {
+        res.status(404).send('Message not found');
+    }
+});
+
+app.listen(8080, () => {
     console.log("Server running at port 8080");
-    
-})
+});
